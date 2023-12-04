6ff3e9a29f2dbb4586cb9e9dbb4f5dbbc77a5898
ManuKaiser <152777663+ManuKaiser@users.noreply.github.com>
2023-12-04 15:11:39 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,10 @@
+# abs pitch > abs roll    pitch runter ist gut
+#       linear = 0
+#       roll + -> links lenken
+#            angular = v            
+#       roll - -> rechts lenken
+#            angular = -v
+#
+#else                            (abs pitch< abs roll)
+#   sin roll * omega = angular
+#   cos abs roll * v = linear

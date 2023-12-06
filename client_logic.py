16820795d084bdaf7bf19484b7d508557cb53546
v1nc24 <86296953+v1nc24@users.noreply.github.com>
2023-12-06 16:12:27 +0100

    Add files via upload

---
+++ client_logic.py
@@ -0,0 +1,25 @@
+
+
+class ClientLogic():
+    def __init__(self):
+        pass
+    def get_next_client_order(self,result):
+        try:
+            match result:
+                case "Abort":
+                    order = None
+                case "Start":
+                    order = "move"
+                case "RFID_reached":
+                    order = "turn"                
+                case "Robot_detected":
+                    order = "follow"
+                case "Turn_succesfull":
+                    order = "move"
+                case "Follow_succesfull":
+                    order = None
+                case "Canceled":
+                    order = None
+            return order    
+        except:
+            return None

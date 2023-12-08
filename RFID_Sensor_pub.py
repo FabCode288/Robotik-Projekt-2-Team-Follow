8d82813fe2cef260a448b9a5be7747619cad8578
ManuKaiser <152777663+ManuKaiser@users.noreply.github.com>
2023-12-08 16:20:22 +0100

    Update RFID_Sensor_pub.py

--- RFID_Sensor_pub.py
+++ RFID_Sensor_pub.py
@@ -11,24 +11,33 @@ class Minimal_RFID_Publisher(Node):
 
     def __init__(self):
         super().__init__('rfid_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic_rfid', 10)
+        self.publisher_ = self.create_publisher(String, 'rfid_topic', 10)
         timer_period = 0.2  # pulishs Data String with 5Hz
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
         self.rfid_id #The id of the rfid-chip
         self.rfid_data #The data of the rfid-chip
 
+        self.i = 0
+
     def timer_callback(self):
-        try:
-            self.rfid_id, self.rfid_data = reader.read() #Reading the rfid-chip
-        except:
-            self.rfid_id, self.rfid_data = None  #If reading the rfid failed the id and data is set to None
-        if(self.rfid_id is not None and self.rfid_data is not None):
+        # try:
+        #     self.rfid_id, self.rfid_data = reader.read() #Reading the rfid-chip
+        # except:
+        #     self.rfid_id, self.rfid_data = None  #If reading the rfid failed the id and data is set to None
+        # if(self.rfid_id is not None and self.rfid_data is not None):
+        #     msg = String()
+        #     msg.data = self.rfid_data
+        #     self.publisher_.publish(msg)
+        #     self.get_logger().info('Publishing: "%s"' % msg.data)
+        if i<200:
             msg = String()
-            msg.data = self.rfid_data
+            msg.data = None
             self.publisher_.publish(msg)
-            self.get_logger().info('Publishing: "%s"' % msg.data)
-            self.i += 1
+            self.i += 1  
+        else
+            msg = String()
+            msg.data = "rfid1"
+            self.publisher_.publish(msg)
 
 def main(args=None):
     rclpy.init(args=args)

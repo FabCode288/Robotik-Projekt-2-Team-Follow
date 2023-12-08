8862730c6ad7241179b14e1503d536dcc39a3d74
ManuKaiser <152777663+ManuKaiser@users.noreply.github.com>
2023-12-08 06:48:13 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -8,3 +8,14 @@
 #else                            (abs pitch< abs roll)
 #   sin roll * omega = angular
 #   cos abs roll * v = linear
+g = [5,5,5,5,5,5,5,5,5,5,3,3,3,3,3,2,2,2,2,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0]
+x_ = 0
+sig = 0
+
+for i in range g.len-1:
+    x_ += g[i]
+
+print(x_)
+
+for j in range g.len-1:
+    sig += (x_-g[j])^2/(x_-1)

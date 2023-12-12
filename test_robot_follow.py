e444b72e17c3de28b08f940a15d39fbf32c56744
ManuKaiser <152777663+ManuKaiser@users.noreply.github.com>
2023-12-12 16:40:33 +0100

    Update test_robot_follow.py

--- test_robot_follow.py
+++ test_robot_follow.py
@@ -25,7 +25,7 @@ Given a new RobotFollow with wanted dist 300 v 1 rot 1
 When the method follow is called with pitch = 0 and roll = 0 and camera dist = 200
 Then the method returns [0.1,0]
 """
-def test_follow_300_1_1():
+def test_follow_200_1_1():
     mover = RobotFollow(300, 1,1)
     assert mover.follow(0,0,200) == [0.1,0]
 
@@ -34,7 +34,7 @@ Given a new RobotFollow with wanted dist 300 v 1 rot 1
 When the method follow is called with pitch = 0 and roll = 0 and camera dist = 400
 Then the method returns [0.1,0]
 """
-def test_follow_300_1_1():
+def test_follow_400_1_1():
     mover = RobotFollow(300, 1,1)
     assert mover.follow(0,0,400) == [1.9,0]
 

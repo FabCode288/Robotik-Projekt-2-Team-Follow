183bff608df673b09230c498c5dcf0a24de7a00b
ManuKaiser <152777663+ManuKaiser@users.noreply.github.com>
2023-12-12 15:16:34 +0100

    Update test_robot_move.py

--- test_robot_move.py
+++ test_robot_move.py
@@ -71,3 +71,23 @@ Then the method returns [0, RobotMove._omega]
 def test_gmp_pi2_minus1():
     mover = RobotMove()
     assert mover.get_movement_pipe(np.pi/2,-1, "None", -1.0) == [0, RobotMove._omega]
+
+"""
+Given a new RobotMove
+When the method get_movement_pipe is called with pitch = pi/2 and roll = -1 and RFID tag = "notNone" and camera dist = -1.0
+Then the method returns None
+"""
+
+def test_gmp_pi2_minus1_notNone():
+    mover = RobotMove()
+    assert mover.get_movement_pipe(np.pi/2,-1, "notNone", -1.0) == None
+
+"""
+Given a new RobotMove
+When the method get_movement_pipe is called with pitch = pi/2 and roll = -1 and RFID tag = "None" and camera dist = 1.0
+Then the method returns None
+"""
+
+def test_gmp_pi2_minus1_dist():
+    mover = RobotMove()
+    assert mover.get_movement_pipe(np.pi/2,-1, "None", 1.0) == None

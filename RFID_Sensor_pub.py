bd5f624b7946b56b30259522e67b545eaf9c28b3
Fabi <134051791+FabCode288@users.noreply.github.com>
2023-12-12 13:00:21 +0100

    Update RFID_Sensor_pub.py

--- RFID_Sensor_pub.py
+++ RFID_Sensor_pub.py
@@ -9,11 +9,15 @@ from std_msgs.msg import String
 
 class Minimal_RFID_Publisher(Node):
 
+    """Creating a new minimal publisher to publish the current value of the RFID sensor with constant frequency
+
+    """
+
     def __init__(self):
         super().__init__('rfid_publisher')
-        self.publisher_ = self.create_publisher(String, 'rfid_topic', 10)
+        self.publisher_ = self.create_publisher(String, 'rfid_topic', 10) #Creating the publisher
         timer_period = 1.0  # pulishs Data String with 5Hz
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.timer = self.create_timer(timer_period, self.timer_callback) #Crating the timer for a current frequency
         # self.rfid_id #The id of the rfid-chip
         # self.rfid_data #The data of the rfid-chip
 
@@ -21,13 +25,13 @@ class Minimal_RFID_Publisher(Node):
 
     def timer_callback(self):
         # try:
-        #     self.rfid_id, self.rfid_data = reader.read() #Reading the rfid-chip
+        #     self.rfid_id, self.rfid_data = reader.read() #Reading the id and the data that is meassured by the rfid-chip
         # except:
         #     self.rfid_id, self.rfid_data = None  #If reading the rfid failed the id and data is set to None
         # if(self.rfid_id is not None and self.rfid_data is not None):
         #     msg = String()
         #     msg.data = self.rfid_data
-        #     self.publisher_.publish(msg)
+        #     self.publisher_.publish(msg) #publishing the data
         #     self.get_logger().info('Publishing: "%s"' % msg.data)
         if self.i<5:
             msg = String()

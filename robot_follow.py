9b9d4eeeb9880ea8a5ea7e22768f3305068de641
ManuKaiser <152777663+ManuKaiser@users.noreply.github.com>
2023-12-13 10:03:52 +0100

    Update robot_follow.py

--- robot_follow.py
+++ robot_follow.py
@@ -7,15 +7,16 @@ class RobotFollow:
 
     def __init__(self, wanted_dist, default_v, default_omega):
         self.wanted_dist = wanted_dist
-        self.default_v = default_v
+        self.last_v = default_v
         self.default_omega = default_omega
         
 
     def follow(self, dist_to_robot, pitch, roll):
         try:
             res = (dist_to_robot-self.wanted_dist)*0.01 #Faktor noch einstellen
-            mov = self.get_movement_pipe(pitch,roll, self.default_v, self.default_omega)
-            mov[0] = mov[0]+res*self.default_v
+            mov = self.get_movement_pipe(pitch,roll, self.last_v, self.default_omega)
+            mov[0] = mov[0]+res*self.last_v
+            self.last_v = mov[0]
             return mov
         except (ValueError, TypeError):
             return None

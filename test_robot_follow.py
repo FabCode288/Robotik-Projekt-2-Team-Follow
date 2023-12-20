e00777726a954f8ad529498b0bff5b1dc247f6bb
ManuKaiser <152777663+ManuKaiser@users.noreply.github.com>
2023-12-20 11:00:09 +0100

    Update test_robot_follow.py

--- test_robot_follow.py
+++ test_robot_follow.py
@@ -3,38 +3,38 @@ import numpy as np
 from robot_follow import RobotFollow 
 
 """
-Given a new RobotFollow with wanted dist 300 v 1 rot 1
+Given a new RobotFollow with wanted dist 30 v 1 rot 1
 When the method follow is called with invalid parameters
 Then the method returns None
 """
 def test_follow_invalid_parameters():
-    mover = RobotFollow(300,1,1)
+    mover = RobotFollow(30,1,1)
     assert mover.follow("a","b","c") == None
 
 """
-Given a new RobotFollow with wanted dist 300 v 1 rot 1
-When the method follow is called with pitch = 0 and roll = 0 and camera dist = 300
+Given a new RobotFollow with wanted dist 30 v 1 rot 1
+When the method follow is called with pitch = 0 and roll = 0 and camera dist = 30
 Then the method returns [1,0]
 """
 def test_follow_300_1_1():
-    mover = RobotFollow(300, 1,1)
-    assert mover.follow(300,0,0) == [1,0]
+    mover = RobotFollow(30, 1,1)
+    assert mover.follow(30,0,0) == [1,0]
 
 """
-Given a new RobotFollow with wanted dist 300 v 1 rot 1
-When the method follow is called with pitch = 0 and roll = 0 and camera dist = 200
+Given a new RobotFollow with wanted dist 30 v 1 rot 1
+When the method follow is called with pitch = 0 and roll = 0 and camera dist = 20
 Then the method returns [0.1,0]
 """
 def test_follow_200_1_1():
-    mover = RobotFollow(300, 1,1)
-    assert mover.follow(200,0,0) == [0,0]
+    mover = RobotFollow(30, 1,1)
+    assert mover.follow(20,0,0) == [0,0]
 
 """
-Given a new RobotFollow with wanted dist 300 v 1 rot 1
-When the method follow is called with pitch = 0 and roll = 0 and camera dist = 400
+Given a new RobotFollow with wanted dist 30 v 1 rot 1
+When the method follow is called with pitch = 0 and roll = 0 and camera dist = 40
 Then the method returns [0.1,0]
 """
 def test_follow_400_1_1():
-    mover = RobotFollow(300, 1,1)
-    assert mover.follow(400,0,0) == [2,0]
+    mover = RobotFollow(30, 1,1)
+    assert mover.follow(40,0,0) == [2,0]
 

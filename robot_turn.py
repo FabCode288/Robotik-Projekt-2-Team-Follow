d34bce163e9da310d161bd3c58719bb674eaff53
v1nc24 <86296953+v1nc24@users.noreply.github.com>
2024-01-10 16:20:54 +0100

    Update robot_turn.py

--- robot_turn.py
+++ robot_turn.py
@@ -1,13 +1,16 @@
 import numpy as np
 import math
-
+#fertiggestellt
 class RobotTurn():
     def __init__(self, start_angle, default_omega):
-        if(start_angle < 0):
-            self.end_angle = start_angle + np.pi
-        else:
-            self.end_angle = start_angle - np.pi
-        self.default_omega = default_omega    
+        try:
+            if(start_angle <= 0):
+                self.end_angle = start_angle + np.pi
+            else:
+                self.end_angle = start_angle - np.pi
+            self.default_omega = default_omega    
+        except:
+            pass
 
     def turn(self, current_angle):
         """
@@ -15,10 +18,12 @@ class RobotTurn():
         If not, it returns the velocity to turn with,
         else it returns None
         """
-        if abs(current_angle - self.end_angle) > 0.02:
-            return [0, -self.default_omega]
-        else:
-            print("else")
+        try:
+            if abs(current_angle - self.end_angle) > 0.05:
+                return [0, -self.default_omega]
+            else:
+                return [0,0]
+        except:
             return None
         
         

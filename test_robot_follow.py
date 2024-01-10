eadaaa690d6a50884a20846b7e09061e7727aac5
ManuKaiser <152777663+ManuKaiser@users.noreply.github.com>
2024-01-10 17:02:19 +0100

    Update test_robot_follow.py

--- test_robot_follow.py
+++ test_robot_follow.py
@@ -15,34 +15,34 @@ def test_follow_invalid_parameters():
 """
 Given a new RobotFollow with wanted dist 30 
 When the method follow is called with parameters 30 0
-Then the method returns [0.5, 0]
+Then the method returns [0.1, 0]
 """
 def test_follow_30_0():
     mover = RobotFollow(30)
     command = mover.follow(30,0)
-    assert command[0] == 0.5
+    assert command[0] == 0.1
     assert command[1] == 0
 
 """
 Given a new RobotFollow with wanted dist 30 
 When the method follow is called with parameters 40 0
-Then the method returns [>0.5, 0]
+Then the method returns [>0.1, 0]
 """
 def test_follow_40_0():
     mover = RobotFollow(30)
     command = mover.follow(40,0)
-    assert command[0] > 0.5
+    assert command[0] > 0.1
     assert command[1] == 0
 
 """
 Given a new RobotFollow with wanted dist 30 
 When the method follow is called with parameters 25 0
-Then the method returns [<0.5, 0]
+Then the method returns [<0.1, 0]
 """
 def test_follow_25_0():
     mover = RobotFollow(30)
     command = mover.follow(25,0)
-    assert command[0] < 0.5
+    assert command[0] < 0.1
     assert command[1] == 0
 
 """

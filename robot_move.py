d494c848d7726ee2ada7d265fe4b370c702abd2f
v1nc24 <86296953+v1nc24@users.noreply.github.com>
2024-01-15 18:09:12 +0100

    Update robot_move.py

--- robot_move.py
+++ robot_move.py
@@ -12,16 +12,16 @@ class RobotMove:
         self.last_dist = 0
 
     def follow_line(self, dist_to_line, v):#rechts positiv
-        if abs(abs(self.last_dist)-abs(dist_to_line))<100:#Zu große Abweichungen abfangen
-            dist_to_line = self.last_dist+50*(self.last_dist/abs(self.last_dist))#Alternativ mit geringerer gerichteter Abweichung weiter machen
+        if abs(abs(self.last_dist)-abs(dist_to_line))>100:#Zu große Abweichungen abfangen
+            dist_to_line = self.last_dist+50*math.copysign(1,self.last_dist)#Alternativ mit geringerer gerichteter Abweichung weiter machen
         if abs(self.last_dist) < abs(dist_to_line)+10:#entfernt sich der Bot von der Linie?
             omega=dist_to_line*0.001 #faktor noch einstellen
             if abs(omega) > 0.5:#zu Hohe Geschwindigkeit abfangen
-                omega = 0.5*(omega/abs(omega)) 
+                omega = 0.5*math.copysign(1,omega) 
         elif abs(self.last_dist) > abs(dist_to_line)+10 and abs(dist_to_line)<100:#bewegt sich der Bot auf die Linie zu und ist nah dran?
             omega=dist_to_line*-0.001 #faktor noch einstellen
             if abs(omega) > 0.5:#zu Hohe Geschwindigkeit abfangen
-                omega = 0.5*(omega/abs(omega)) 
+                omega = 0.5*math.copysign(1,omega) 
         else:#Bot fährt auf der Linie
             omega = 0  
         self.last_dist = dist_to_line

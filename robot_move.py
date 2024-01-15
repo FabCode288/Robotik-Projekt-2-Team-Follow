5bb3f0d518fc97c5c82f5223f60f294d6855e77b
v1nc24 <86296953+v1nc24@users.noreply.github.com>
2024-01-15 19:05:07 +0100

    Update robot_move.py

--- robot_move.py
+++ robot_move.py
@@ -10,27 +10,25 @@ class RobotMove:
 
     def __init__(self):
         self.last_dist = 0
+        
 
     def follow_line(self, dist_to_line, v):#rechts positiv
         if abs(abs(self.last_dist)-abs(dist_to_line))>100:#Zu große Abweichungen abfangen
             dist_to_line = self.last_dist+50*math.copysign(1,self.last_dist)#Alternativ mit geringerer gerichteter Abweichung weiter machen
-        if abs(dist_to_line)<100:
+        if abs(dist_to_line)<100:#Bot ist nah an der Linie
             if abs(self.last_dist) < abs(dist_to_line): #entfernt sich der Bot von der Linie?
                 omega=dist_to_line*0.001 #faktor noch einstellen
-                if abs(omega) > 0.5:#zu Hohe Geschwindigkeit abfangen
-                    omega = 0.5*math.copysign(1,omega) 
             elif abs(self.last_dist) > abs(dist_to_line):#bewegt sich der Bot auf die Linie zu und ist nah dran?
                 omega=dist_to_line*-0.001 #faktor noch einstellen
-                if abs(omega) > 0.5:#zu Hohe Geschwindigkeit abfangen
-                    omega = 0.5*math.copysign(1,omega)
             else:#fährt parallel
                 omega =0
-        else:
-            if abs(self.last_dist) < abs(dist_to_line)+10: #entfernt sich der Bot von der Linie?
-                omega=dist_to_line*0.001 #faktor noch einstellen
-                if abs(omega) > 0.5:#zu Hohe Geschwindigkeit abfangen
-                    omega = 0.5*math.copysign(1,omega)         
+        else:#Bot ist nicht nah an der Linie
+            if abs(self.last_dist) < abs(dist_to_line)+10: #fährt der Bot auf die Linie zu?
+                omega=dist_to_line*0.001 #faktor noch einstellen       
             else:#Bot fährt auf Linie zu
                 omega = 0  
+
         self.last_dist = dist_to_line
+        if abs(omega) > 0.5:#zu Hohe Geschwindigkeit abfangen
+                    omega = 0.5*math.copysign(1,omega) 
         return[v, omega] 

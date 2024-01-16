4b189295be193ff4a21c602ad3c5dd87f8340e57
ManuKaiser <152777663+ManuKaiser@users.noreply.github.com>
2024-01-16 12:46:23 +0100

    Update robot_move.py

--- robot_move.py
+++ robot_move.py
@@ -14,22 +14,25 @@ class RobotMove:
         
 
     def follow_line(self, dist_to_line, v):#rechts positiv
-        if abs(abs(self.last_dist)-abs(dist_to_line))>100:#Zu große Abweichungen abfangen
-            dist_to_line = self.last_dist+50*math.copysign(1,self.last_dist)#Alternativ mit geringerer gerichteter Abweichung weiter machen
-        if abs(dist_to_line)<100:#Bot ist nah an der Linie
-            if abs(self.last_dist) < abs(dist_to_line): #entfernt sich der Bot von der Linie?
-                omega=dist_to_line*0.001 #faktor noch einstellen
-            elif abs(self.last_dist) > abs(dist_to_line):#bewegt sich der Bot auf die Linie zu?
-                omega=dist_to_line*-0.001 #faktor noch einstellen
-            else:#fährt parallel
-                omega =0
-        else:#Bot ist nicht nah an der Linie
-            if abs(self.last_dist) < abs(dist_to_line)+10: #fährt der Bot auf die Linie zu?
-                omega=dist_to_line*0.001 #faktor noch einstellen       
-            else:#Bot fährt auf Linie zu
-                omega = 0  
-
-        self.last_dist = dist_to_line
-        if abs(omega) > self.max_omega:#zu Hohe Geschwindigkeit abfangen
-                    omega = self.max_omega*math.copysign(1,omega) 
-        return[v, omega] 
+        try:
+            if abs(abs(self.last_dist)-abs(dist_to_line))>100:#Zu große Abweichungen abfangen
+                dist_to_line = self.last_dist+50*math.copysign(1,self.last_dist)#Alternativ mit geringerer gerichteter Abweichung weiter machen
+            if abs(dist_to_line)<100:#Bot ist nah an der Linie
+                if abs(self.last_dist) < abs(dist_to_line): #entfernt sich der Bot von der Linie?
+                    omega=dist_to_line*0.001 #faktor noch einstellen
+                elif abs(self.last_dist) > abs(dist_to_line):#bewegt sich der Bot auf die Linie zu?
+                    omega=dist_to_line*-0.001 #faktor noch einstellen
+                else:#fährt parallel
+                    omega =0
+            else:#Bot ist nicht nah an der Linie
+                if abs(self.last_dist) < abs(dist_to_line)+10: #fährt der Bot auf die Linie zu?
+                    omega=dist_to_line*0.001 #faktor noch einstellen       
+                else:#Bot fährt auf Linie zu
+                    omega = 0  
+    
+            self.last_dist = dist_to_line
+            if abs(omega) > self.max_omega:#zu Hohe Geschwindigkeit abfangen
+                        omega = self.max_omega*math.copysign(1,omega) 
+            return[v, omega] 
+        except:
+            return None

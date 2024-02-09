43b5d756eab9890a3a1ecd9e0e3e6194082b1a22
v1nc24 <86296953+v1nc24@users.noreply.github.com>
2024-02-09 10:36:01 +0100

    Update robot_move.py

--- robot_move.py
+++ robot_move.py
@@ -24,7 +24,7 @@ class RobotMove:
         if dist_to_line == 66666:
             return [v,0.0]
         else:
-            derivative = (dist_to_line - self.last_dist)/(step-self.last_step
+            derivative = (dist_to_line - self.last_dist)/(step-self.last_step)
             self.last_step=step
                                                           
             omega = -1*(dist_to_line*self.kP + derivative*self.kD) 
